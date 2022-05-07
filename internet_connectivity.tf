5303de11796109f5244c54799a0cc66dacd41276
collins <nassycollins@gmail.com>
2022-05-07 06:06:15 -0400

    updated

--- internet_connectivity.tf
+++ internet_connectivity.tf
@@ -1,25 +1,33 @@
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "collinsnassy_gw" {
+  vpc_id = aws_vpc.collins_vpc.id
 
   tags = {
     Name = "main"
   }
 }
 
-resource "aws_route_table" "example" {
-  vpc_id = aws_vpc.example.id
+resource "aws_route_table" "collinsnassy_rt" {
+  vpc_id = aws_vpc.collins_vpc.id
 
   route {
     cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.example.id
+    gateway_id = aws_internet_gateway.collinsnassy_gw.id
   }
 
   route {
     ipv6_cidr_block        = "::/0"
-    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+    egress_only_gateway_id = aws_egress_only_internet_gateway.collins_egressonly_igw.id
   }
 
   tags = {
     Name = "example"
+  }
+}
+
+resource "aws_egress_only_internet_gateway" "collins_egressonly_igw" {
+  vpc_id = aws_vpc.collins_vpc.id
+
+  tags = {
+    Name = "main"
   }
 }
